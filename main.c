8405218a3913f5bdcc60569900a1027cef9dab1e
Your Name <you@example.com>
2019-01-15 16:53:22 +0800

    构建敏感字系统框架

--- main.c
+++ main.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 
-int main(){
+char arr[1024];
+int index = 0;
+int main()
+{
 
-    printf("hello world!");
+    printf("minganciguanlixitong\n");
+    while(1)
+    {printf("1---tianjiaminganzifu\n");
+    printf("2---tianjiaminganzifu\n");
+    printf("3---tianjiaminganzifu\n");
+    printf("4---tianjiaminganzifu\n");
+    printf("5---tianjiaminganzifu\n");
+   
+    printf("qingshurubianhao\n");
+   
+    char code;
+    scanf("%d",&code);
+
+    if(code == 1){
+
+
+    }
+    if(code == 2){
+        
+    }
+    if(code == 3){
+        
+    }
+    if(code == 4){
+        
+    }
+    if(code == 5){
+        printf("jieshu\n");
+        break;
+        
+    }
+
+
+    
+    
+    }
+   
+
+
 
     return 0;
-
 }

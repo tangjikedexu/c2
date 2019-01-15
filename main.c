54fd6b5b08b9e5183d6a199e20d75f541d5a4934
Your Name <you@example.com>
2019-01-15 18:40:45 +0800

    添加删除敏感字符功能

--- main.c
+++ main.c
@@ -6,55 +6,54 @@ int main()
 {
 
     printf("minganciguanlixitong\n");
-    while(1)
+    while (1)
     {
-    printf("1---tianjiaminganzifu\n");
-    printf("2---tianjiaminganzifu\n");
-    printf("3---tianjiaminganzifu\n");
-    printf("4---tianjiaminganzifu\n");
-    printf("5---tianjiaminganzifu\n");
-   
-    printf("qingshurubianhao\n");
-   
-    char code;
-    scanf("%d",&code);
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuzuihouyigeminganzifu\n");
+        printf("3---chakansuoyouminganzifu\n");
+        printf("4---tihuanneirongzhongdeminganzifu\n");
+        printf("5---tuichu\n");
 
-    if(code == 1){
-      printf("qingshuruyaoyianjiademinganzifu\n");
-      char m;
-      scanf("%c",&m);
-      scanf("%c",&m);
-      arr[index]=m;
-      index ++;
-      
-      printf("tianjiachenggong,dianjihuichejixu\n");
-      char x;
-      scanf("%c",&x);
-      scanf("%c",&x);
+        printf("qingshurubianhao\n");
 
-    }
-    if(code == 2){
-        
-    }
-    if(code == 3){
-        
-    }
-    if(code == 4){
-        
-    }
-    if(code == 5){
-        printf("jieshu\n");
-        break;
-        
-    }
+        char code;
+        scanf("%d", &code);
 
+        if (code == 1)
+        {
+            printf("qingshuruyaoyianjiademinganzifu\n");
+            char m;
+            scanf("%c", &m);
+            scanf("%c", &m);
+            arr[index] = m;
+            index++;
 
-    
-    
+            printf("tianjiachenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
+        if (code == 2)
+        {
+            printf("shanchuzuihouyigeminganzifu\n");
+            index--;
+            printf("shanchuchenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("jieshu\n");
+            break;
+        }
     }
-   
-
-
 
     return 0;
 }

0bf208dcb121211a6e16312d7f2bfd9f4930d0b4
Your Name <you@example.com>
2019-01-15 19:42:44 +0800

    添加替换敏感字符系统

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 
 char arr[1024];
 int index = 0;
+int isMingan(char x){
+    int count = 0;
+    for(int i=0;i<index;i++){
+        if(x==arr[i]){
+            count++;
+        }
+    }
+    if(count == 0){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
 int main()
 {
 
@@ -57,6 +71,26 @@ int main()
         }
         if (code == 4)
         {
+            printf("qingshuruyiduanyingwen \n");
+            char neirong[1024];
+            scanf("%s", neirong);
+            for(int i=0;1;i++)
+            {
+                if (neirong[i] == '\0')
+                {
+                    break;
+                }
+                if (isMingan(arr[i]) == 1)
+                {
+                    neirong[i] = '*';
+                }
+                printf("%c", neirong[i]);
+            }
+            printf("\n");
+            printf("tihuanwancheng,dianjihuichejixu \n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 5)
         {
